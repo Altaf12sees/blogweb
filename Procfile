79925fb190ca2446f4cc5fe90ebaf0e9fc3c7310
Altaf12sees <altaf12sees@gmail.com>
2021-04-30 22:11:52 +0400

    Initial Commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn wtsolweb.wsgi
---
+++ api/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from movies.models import Movies
+# from tastypie.resources import ModelResource
+
+
+# class MovieResource():
+#     class Meta:
+#         queryset = Movies.objects.all()
+#         resource_name = 'movies'
