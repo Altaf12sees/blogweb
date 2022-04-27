80dd453091cabb7d7a1144df8436099cdc768b48
Altaf12sees <altaf12sees@gmail.com>
2022-04-28 01:12:33 +0400

    initial

--- api/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from movies.models import Movies
-# from tastypie.resources import ModelResource
-
-
-# class MovieResource():
-#     class Meta:
-#         queryset = Movies.objects.all()
-#         resource_name = 'movies'
